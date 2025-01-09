f67e12152739827cdb74f3503e310271b507014a
junaidmfaisal <junaidmfaisal@gmail.com>
2025-01-09 23:17:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
-import Contact from './pages/contact'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
